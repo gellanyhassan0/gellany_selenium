29df2b811e2caa390a0af1e0c8d6480fb1f971ee
Elgilany Hassan <gellanyhassan0@gmail.com>
2022-06-24 09:15:49 +0000

    Add files via upload

---
+++ selenium_chromedriver.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt-get reinstall chromium -y
+wget https://chromedriver.storage.googleapis.com/102.0.5005.61/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+cp -r chromedriver /usr/bin/chromedriver
+
+python3 selenium_chromedriver_test.py
