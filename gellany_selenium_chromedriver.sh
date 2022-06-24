028d1b425410382e7dde1af88ababe5140c8ecee
Elgilany Hassan <gellanyhassan0@gmail.com>
2022-06-24 19:07:46 +0000

    Update gellany_selenium_chromedriver.sh

--- gellany_selenium_chromedriver.sh
+++ gellany_selenium_chromedriver.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+RUN apt-get install -y libglib2.0-0 libnss3 libgconf-2-4 libfontconfig1
 apt-get install chromium -y
 wget https://chromedriver.storage.googleapis.com/102.0.5005.61/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
