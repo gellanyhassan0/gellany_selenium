0cb2aacb63031e26e1f94c313f41836e8037bb8c
Elgilany Hassan <gellanyhassan0@gmail.com>
2022-10-20 14:03:01 +0000

    Update gellany_selenium_chromedriver.sh

--- gellany_selenium_chromedriver.sh
+++ gellany_selenium_chromedriver.sh
@@ -3,7 +3,11 @@
 RUN apt-get install -y libglib2.0-0 libnss3 libgconf-2-4 libfontconfig1
 apt-get install chromium -y
 apt-get install chromium-driver -y
-cp -r chromedriver /usr/bin/
+
+#########################old version of chromium-driver setup of version 102####################
+#wget https://chromedriver.storage.googleapis.com/102.0.5005.61/chromedriver_linux64.zip -O chromedriver_linux64.zip
+#unzip chromedriver_linux64.zip
+#cp -r chromedriver /usr/bin/
 
 apt-get install python3 -y
 apt-get install python3-pip -y
